2611bd70ce3d308e406f6f13de552ae6037be23c
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-05-23 13:14:45 +0300

    Update Searches Controller to include search history

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,8 @@
 class SearchesController < ApplicationController
 
     def index
+        user_ip_address = request.remote_ip
+        @search_history = Search.search_history(user_ip_address)
     end
 
     def create
@@ -13,9 +15,9 @@ class SearchesController < ApplicationController
     def resolve_queries
         user_ip_address = request.remote_ip
         final_query = params[:query]
-        incomplete_searches = Search.where(user_ip_address: user_ip_address, completed: false)
+        @incomplete_searches = Search.incomplete_search(user_ip_address)
         Search.create(user_ip_address: user_ip_address, query: final_query, completed: true)
-        incomplete_searches.destroy_all
+        @incomplete_searches.destroy_all
         render json: { status: '200' }
     end
 
