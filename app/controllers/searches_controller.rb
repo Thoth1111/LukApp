f3135992c30457e904218d6a5937d6e48465ee58
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-05-23 18:30:09 +0300

    Update resolve_queries method in search controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -16,7 +16,13 @@ class SearchesController < ApplicationController
         user_ip_address = request.remote_ip
         final_query = params[:query]
         @incomplete_searches = Search.incomplete_search(user_ip_address)
-        Search.create(user_ip_address: user_ip_address, query: final_query, completed: true)
+        saved_query = Search.find_by(query: final_query, user_ip_address: user_ip_address, completed: true)
+        if saved_query
+            saved_query.count += 1
+            saved_query.save
+        else
+            Search.create(user_ip_address: user_ip_address, query: final_query, completed: true, count: 1)
+        end
         @incomplete_searches.destroy_all
         render json: { status: '200' }
     end
