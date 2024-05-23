b0a3294fb4fdc89fe2a0f7e4c3a238c9a3278fd5
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-05-23 22:57:37 +0300

    Update searches controller and add method for deleting incomplete searches

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,24 +7,30 @@ class SearchesController < ApplicationController
 
     def create
         user_ip_address = request.remote_ip
-        query = params[:query]
-        Search.create!( user_ip_address: user_ip_address, query: query, completed: false)
-        render json: { status: '200' }
+        query = params[:query].downcase
+        Search.create( user_ip_address: user_ip_address, query: query, completed: false)
+        matching_searches = Search.where("query LIKE ?", "%#{query}%").where(completed: true).pluck(:query)
+        render json: { status: '200', suggestions: matching_searches }
     end
 
     def resolve_queries
         user_ip_address = request.remote_ip
-        final_query = params[:query]
+        final_query = params[:query].downcase
         @incomplete_searches = Search.incomplete_search(user_ip_address)
         saved_query = Search.find_by(query: final_query, user_ip_address: user_ip_address, completed: true)
         if saved_query
             saved_query.count += 1
             saved_query.save
+            new_query = saved_query
         else
-            Search.create(user_ip_address: user_ip_address, query: final_query, completed: true, count: 1)
+            new_query = Search.create(user_ip_address: user_ip_address, query: final_query, completed: true, count: 1)
         end
         @incomplete_searches.destroy_all
-        render json: { status: '200' }
+        render json: { status: '200', new_query: new_query }
     end
 
+    def delete_incomplete_queries
+        Search.delete_incomplete_queries
+        render json: { status: '200' }
+    end
 end
