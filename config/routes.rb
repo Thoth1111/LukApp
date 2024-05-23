ecee8831e6e735c9624a66098b27633d65df8188
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-05-23 13:13:32 +0300

    Update Collection actions in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :searches, only: [:index, :create]
+  resources :searches, only: [:index, :create] do
+    collection do
+      post :resolve_queries
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
