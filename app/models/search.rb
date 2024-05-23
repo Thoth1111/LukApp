ec256ad634ddca0327010a3e9bfd2a4dcc667c47
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-05-23 18:29:45 +0300

    Add validations to search model and update search history method

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,14 +1,14 @@
 class Search < ApplicationRecord
+    validates :user_ip_address, presence: true
+    validates :query, presence: true
+
     #Method to retrieve incomplete searches based on a user's IP address
     def self.incomplete_search(ip_address)
         self.where(user_ip_address: ip_address, completed: false)
     end
     
-    #Method to retrieve a user's ranked query history based on their IP address
+    #Method to retrieve a user's ranked query history based on count and IP address
     def self.search_history(ip_address)
-        self.where(user_ip_address: ip_address, completed: true)
-            .select('query, COUNT(query) as query_count')
-            .group(:query)
-            .order('query_count DESC')
+        self.where(user_ip_address: ip_address, completed: true).order('count DESC')
     end
 end
