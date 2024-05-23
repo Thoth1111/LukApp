62535204d888bb9ebbaf16a10c71308dab68dc26
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-05-23 11:08:59 +0300

    Add 'create' & 'resolve_queries' methods in Searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,22 @@
 class SearchesController < ApplicationController
+
+    def index
+    end
+
+    def create
+        user_ip_address = request.remote_ip
+        query = params[:query]
+        Search.create!( user_ip_address: user_ip_address, query: query, completed: false)
+        render json: { status: '200' }
+    end
+
+    def resolve_queries
+        user_ip_address = request.remote_ip
+        final_query = params[:query]
+        incomplete_searches = Search.where(user_ip_address: user_ip_address, completed: false)
+        Search.create(user_ip_address: user_ip_address, query: final_query, completed: true)
+        incomplete_searches.destroy_all
+        render json: { status: '200' }
+    end
+
 end
